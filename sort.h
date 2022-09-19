fcc69c6453a93a33f08005dd4211fdf416412a69
darbumas <2314@holbertonschool.com>
2022-09-19 13:10:14 -0500

    heap sort

--- sort.h
+++ sort.h
@@ -22,7 +22,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void radix_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
-void sort_recursive(int *, int *, int, int);
-void merge_arrays(int *, int *, int, int, int);
 #endif
